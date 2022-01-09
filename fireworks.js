dadc7d845503cde9748d0b0f40e92e17a28b6bad
mlflanagan <72321981+mlflanagan@users.noreply.github.com>
2022-01-09 03:08:27 -0500

    Update fireworks.js
    Refactored to use Array.prototype.filter in model.update method.

--- fireworks.js
+++ fireworks.js
@@ -1,10 +1,9 @@
-/*jslint long:true, this:true, browser:true, unordered:true, devel:true, for: true */
+/*jslint long:true, this:true browser:true, unordered:true */
 /*jshint esversion: 6 */
 
 /*
 2022-01-03
 Inspired by https://stackoverflow.com/q/43498923
-TODO: Use Array.prototype.filter in model update method
 */
 
 (function () {
@@ -20,46 +19,48 @@ TODO: Use Array.prototype.filter in model update method
             this.x = x;
             this.y = y;
 
-            // varying dx and dy sets particle direction
             this.dx = randBetween(Config.particles.minSpeed, Config.particles.maxSpeed);
             this.dy = randBetween(Config.particles.minSpeed, Config.particles.maxSpeed);
 
-            this.size = randBetween(Config.particles.minSize, Config.particles.maxSize);
+            this.size = randBetween(Config.particles.minSize, Config.particles.maxSize + 1);
 
+            /* set color in Explosion class
             this.r = randBetween(0, 256);
             this.g = randBetween(0, 256);
-            this.b = randBetween(0, 256);
+            this.b = randBetween(0, 256); */
         }
 
         this.Explosion = function (x, y) {
             this.particles = [];
+            // set all particles to the same color
+            let red = randBetween(0, 256);
+            let green = randBetween(0, 256);
+            let blue = randBetween(0, 256);
             /* Note: could also use the ES6 spread operator ("..."), like this:
                [...new Array(Config.particles.perExplosion).keys()].forEach(function () { */
             Array.from(new Array(Config.particles.perExplosion).keys()).forEach(function () {
                 let newParticle = new Particle(x, y);
+                newParticle.r = red;
+                newParticle.g = green;
+                newParticle.b = blue;
                 this.particles.push(newParticle);
             }, this);
         };
 
         this.update = function () {
-            for (let i = 0; i < this.explosions.length; i += 1) {
-                let explosion = this.explosions[i];
-                for (let j = 0; j < explosion.particles.length; j += 1) {
-                    let particle = explosion.particles[j];
+            this.explosions.forEach(function (explosion) {
+                explosion.particles.forEach(function (particle) {
                     particle.x += particle.dx;
                     particle.y += particle.dy;
                     particle.size -= 0.1;
-                    if (particle.size <= 0.0) {
-                        explosion.particles.splice(j, 1);
-                        j -= 1;
-                    }
-                }
-
-                if (explosion.particles.length === 0) {
-                    this.explosions.splice(i, 1);
-                    i -= 1;
-                }
-            }
+                }, this);
+                explosion.particles = explosion.particles.filter(function (particle) {
+                    return (particle.size > 0.0);
+                });
+            }, this);
+            this.explosions = this.explosions.filter(function (explosion) {
+                return (explosion.particles.length > 0);
+            });
         };
     }
 
@@ -100,14 +101,12 @@ TODO: Use Array.prototype.filter in model update method
             model.explosions.push(newExplosion);
         };
 
-        // maintain binding for event listener
-        // this.clickHandler = this.clickHandler.bind(this);
         window.addEventListener("click", this.clickHandler);
     }
 
     // GAME ENGINE ----------------------------------------------------
 
-    // config options
+    // options
     const Config = {
         fps: 60,
         background: "black",
@@ -143,7 +142,5 @@ TODO: Use Array.prototype.filter in model update method
     let view = new View();
     let controller = new Controller(); // ignore lint 'unused' warning
 
-    // pass high precision time for the first call to mainloop
-    // (this is what requestAnimationFrame uses instead of Date.now())
     mainloop(performance.now());
 }());
